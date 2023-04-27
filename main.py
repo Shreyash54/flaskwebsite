cf8da8cf047bb024f6dfb2df807cc2a180510b6c
Shreyash54 <91052115+Shreyash54@users.noreply.github.com>
2023-04-27 19:21:34 +0000

    front end

--- main.py
+++ main.py
@@ -1,11 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+JOBS = [{
+  'id': 1,
+  'title': 'DATA ANALYST',
+  'location': 'Bengaluru ,India',
+  'salary': 'Rs 1,00,000',
+}, {
+  'id': 2,
+  'title': 'DATA SCIENTIST',
+  'location': 'Delhi ,India',
+  'salary': 'Rs 2,00,000',
+}, {
+  'id': 3,
+  'title': 'DATA ANALYST',
+  'location': 'Mumbai ,India',
+  'salary': 'Rs 14,00,000',
+}, {
+  'id': 4,
+  'title': 'DATA MINER',
+  'location': 'MP ,India',
+  'salary': 'Rs 11,00,000',
+}]
 
 
 @app.route('/')
 def hello():
-  return 'hello fucker  gok no ok'
+  return render_template('home.html', jobs=JOBS , companyname = 'Harry')
 
 
 if __name__ == "__main__":
